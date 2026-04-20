edb415ba82a3e2818d513706effd801ebe453f13
trsyjr <trsyjr@dswd.gov.ph>
2026-04-20 14:39:57 +0800

    Clear Comments

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 1. Import this
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 2. Add this here
+    tailwindcss(),
   ],
 })
